dba592abc2fd145406210d460ba9272e57eac1b2
Florian Mauracher <florian.mauracher@gmx.net>
2017-04-15 03:09:21 +0200

    sense-utils: Workaround stdout buffer for more frequent db updates

--- air-sense/utils/serial_to_sqlite.sh
+++ air-sense/utils/serial_to_sqlite.sh
@@ -8,4 +8,12 @@ if [ "$1" != "-c" ]; then
 fi
 
 # Read serial
-picocom -b 115200 /dev/tty.usbmodem2477801 | tee $fname.log | perl -lne 'm/counter=.+,temperature=(?<t>.+),humidity=(?<h>.+),pressure=(?<p>.+),mq135=(?<m>.+),iaqs=00,co2=(?<c>.+),tvoc=(?<v>.+),chk=.+/; print "INSERT INTO sensors (temperature, humidity, pressure, mq135, iaqs, co2, tvoc) VALUES ($+{t},$+{h},$+{p},$+{m},0,$+{c},$+{v});"' | sqlite3 $fname.sqlite
+picocom -b 115200 /dev/tty.usbmodem2477801 | tee $fname.log &
+sleep 1
+
+# Write to sqlite
+tail -f $fname.log | while read LINE; do
+	echo $LINE |
+	perl -lne 'm/counter=.+,temperature=(?<t>.+),humidity=(?<h>.+),pressure=(?<p>.+),mq135=(?<m>.+),iaqs=(?<i>.+),co2=(?<c>.+),tvoc=(?<v>.+),chk=.+/; print "INSERT INTO sensors (temperature, humidity, pressure, mq135, iaqs, co2, tvoc) VALUES ($+{t},$+{h},$+{p},$+{m},$+{i},$+{c},$+{v});"' |
+	sqlite3 $fname.sqlite
+done
