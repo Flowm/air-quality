8f7de7fb27f5b95073149b307c6be2413ad858d6
Florian Mauracher <florian.mauracher@gmx.net>
2017-10-30 00:36:15 +0100

    Only add available sensor values

--- air-sense/AQSensor.cpp
+++ air-sense/AQSensor.cpp
@@ -16,17 +16,25 @@ const char* AQSensor::format(int counter) {
     snprintf(buf, BUFSZ, "cnt=%05d", counter);
     strlcat(line, buf, LINESZ);
 
-    snprintf(buf, BUFSZ, ",temp=%04.2f,humi=%05.3f,pres=%07.2f", temperature, humidity, pressure);
-    strlcat(line, buf, LINESZ);
+    if (temperature) {
+        snprintf(buf, BUFSZ, ",temp=%04.2f,humi=%05.3f,pres=%07.2f", temperature, humidity, pressure);
+        strlcat(line, buf, LINESZ);
+    }
 
-    snprintf(buf, BUFSZ, ",gasr=%05.0f", gas_resistance);
-    strlcat(line, buf, LINESZ);
+    if (gas_resistance) {
+        snprintf(buf, BUFSZ, ",gasr=%05.0f", gas_resistance);
+        strlcat(line, buf, LINESZ);
+    }
 
-    snprintf(buf, BUFSZ, ",amq=%04d", analog_mq135);
-    strlcat(line, buf, LINESZ);
+    if (analog_mq135) {
+        snprintf(buf, BUFSZ, ",amq=%04d", analog_mq135);
+        strlcat(line, buf, LINESZ);
+    }
 
-    snprintf(buf, BUFSZ, ",ali=%04d", analog_light);
-    strlcat(line, buf, LINESZ);
+    if (analog_light) {
+        snprintf(buf, BUFSZ, ",ali=%04d", analog_light);
+        strlcat(line, buf, LINESZ);
+    }
 
     // Calculate checksum for transmission
     int sum = 0;
